ef6d9a9673b8ff443438c744099a8f5ed313093b
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2026-05-23 21:20:18 +0530

    develop

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-5xl bg-[#fefefe] flex-col">
+    <div className="min-h-screen flex items-center justify-center text-[4.5em] md:text-[10em] bg-[#fefefe] flex-col leading-16 md:leading-32">
       <p className="absolute top-0 left-6 font-geist text-sm md:text-xl tracking-tigher font-medium flex items-center justify-center">madewebs ©</p>
        Under Development
        <p className="italic font-medium"> Back Soon</p>
